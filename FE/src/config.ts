1b368e0403ddef149126c4c18a8194fd8818b269
김기현 <luke0985>
2026-07-07 15:42:47 +0900

    feat(임시) : 로컬 env 매핑용 config 추가

---
+++ FE/src/config.ts
@@ -0,0 +1,3 @@
+export const API_BASE_URL = import.meta.env.VITE_API_BASE_URL ?? "";
+export const NAVER_MAP_CLIENT_ID = import.meta.env.VITE_NAVER_MAP_CLIENT_ID ?? "";
+export const NAVER_MAP_APPLICATION_NAME = import.meta.env.VITE_NAVER_MAP_APPLICATION_NAME ?? "piumteo-dev";
